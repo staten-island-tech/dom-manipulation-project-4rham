c297a5ef0ef7d244e65b8fd405b1fec04c89789d
4rham <arhambajwa24@gmail.com>
2022-11-28 06:03:45 -0500

    lol

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const DOMSelectors = {
     genre: document.getElementById("genre"),
     platform: document.getElementById("platform"),
     button: document.getElementById("button"),
-    section: document.getElementById("section"),
+    form: document.querySelector("#form"),
+    container: document.querySelector("#container"),
+    remove: document.querySelectorAll("#remove"),
 };
 
-DOMSelectors.form.addEventListener("submit", function (){
+DOMSelectors.form.addEventListener("submit", function () {
     event.preventDefault();
     let box = {};
     box.name = document.getElementById("name");
@@ -21,10 +23,23 @@ DOMSelectors.form.addEventListener("submit", function () {
     DOMSelectors.container.insertAdjacentHTML(
         "beforehand",
         `<div class="card">
-        <img src="https://media.tenor.com/SSY2V0RrU3IAAAAd/rick-roll-rick-rolled.gif" alt="rickoll" id="game-cover">
+        <img src="https://cdn.vox-cdn.com/thumbor/WR9hE8wvdM4hfHysXitls9_bCZI=/0x0:1192x795/1400x1400/filters:focal(596x398:597x399)/cdn.vox-cdn.com/uploads/chorus_asset/file/22312759/rickroll_4k.jpg" id="game-cover">
         <p> Dear $(name), you requested a $(genre) game on $(platform), error occured due to you being "too broke", so take this L you broke loser go buy an actual game hahahahahhahahahhah </p>
         </div>`
     );
 });
 
-DOMSelectors.form.addEventListener
+DOMSelectors.form.addEventListener("submit", function () {
+    DOMSelectors.name.value = "";
+    DOMSelectors.genre.value = "";
+    DOMSelectors.platform.value = "";
+});
+
+function remove() {
+    let remove = document.querySelectorAll("#remove");
+    remove.forEach((button) => {
+        button.addEventListener("submit", function () {
+            remove.parentElement.remove();
+        });
+    });
+};
